70fb2075948a0171f2f20db4e64d81a1c45b29a7
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-06-14 10:20:32 -0300

    Update grass.cs

--- Assets/Scripts/grass.cs
+++ Assets/Scripts/grass.cs
@@ -6,9 +6,9 @@ public class grass : MonoBehaviour
 {
     public ParticleSystem fxHit;
     private bool isCutted;
-    void GetHit(int amount)
+    void GetHit(int amount) 
     {
-        if (!isCutted)
+        if (!isCutted)// se for verdadeiro o ataque do player no obejto, ele diminui  scale do objeto  e dispare um efeito
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
             fxHit.Emit(10);
