0d2aca1da23c3b9bf3a961d60b01539ac4699ed4
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-06-14 09:55:34 -0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,9 @@ public enum enemyState
 }
 
 public class GameManager : MonoBehaviour
-{
+{    // pegando player
     public Transform player;
-
+    // ia do npc, variaves de pontos onde o npc vai patrulhar, e a distacia do ataque, tempo de alerta, tempo de ataque pra ataque, e a velocidade que segui olhando o player
     [Header("ENEMY IA")]
     public float slimeIdleWaitTime;
     public Transform[] slimeWayPoints;
